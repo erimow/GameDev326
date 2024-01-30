5cb75fa3ce063dbefd9a3b49684fa087eca548a5
Erik <ermorris@csumb.edu>
2024-01-29 19:21:14 -0800

    Solar System and Project 0 mostly complete

---
+++ Solar system/Assets/Scenes/Scripts/Oscilator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscilator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Vector3 startPos;
+    void Start()
+    {
+        // startPos = GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Transform transform = GetComponent<transform>();
+        // transform.position.x += .5;
+    }
+}
